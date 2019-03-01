6ddba71cf0ca99500a8f84e15b25313fc6a92995
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-03-01 23:10:53 +0000

    Made the Reply Keyboard work; Formatted the starting response; Added a default message for unrecognized commands (will be expanded later).

--- Config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-define("BOT_API_KEY", "679076024:AAEVanzd0gW3ArzjZ51n36rhY6ubjhPMaYs");
-define("DEBUG", false);
--- index.php
+++ index.php
@@ -14,20 +14,31 @@ if (DEBUG) {
 /* Create a new Telegram bot */
 $bot = new \TelegramBot\Api\Client(BOT_API_KEY);
 
-/* TODO: Keyboard suggestions */
-// $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([ [ "/start", "/quote" ] ], true);
-
 /* A sample start command */
 $bot->command("start", function($message) use ($bot) {
-    $answer = "Welcome to Dijkstra's Shortest Quote. \nI hope that I will soon find the shortest path to your heart. \xF0\x9F\x92\x96 \n\n Type in /quote to see a random quote.";
-    $bot->sendMessage($message->getChat()->getId(), $answer);
+    /* A list of keyboard commands */
+    $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([ [ "/quote" ] ], true, true);
+    $answer = "Hi, ".$message->getChat()->getFirstName().". ".
+        "Welcome to Dijkstra's Shortest Quote. ".
+        "I hope that I will soon find the shortest path to your heart. \xF0\x9F\x92\x96 \n\n". 
+        "Type in /quote to see a random quote.";
+    $bot->sendMessage($message->getChat()->getId(), $answer, null, false, null, $keyboard);
 });
 
-/* Command to fetch the quote */
+/* Command to fetch a random quote */
 $bot->command("quote", function($message) use ($bot) {
     $qp = new QuotePicker();
     $answer = $qp->get_quote();
     $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+/* TODO: Implement other functionalities for other types of messages. For now, return an "Unrecognized" message. */
+/* Reference: https://core.telegram.org/bots/api */
+$bot->on(function($update) use ($bot) {
+    $message = $update->getMessage();
+    $bot->sendMessage($message->getChat()->getId(), "This is an unrecognized command.");
+}, function ($message) use ($name) {
+    return true;
 });
 
 /* Run the bot */
